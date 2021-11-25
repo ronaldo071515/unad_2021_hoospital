3b100244a3c87f39a5b93eaf36ef5c9aedf0bf16
Ronaldo Torres <rtorres66@misena.edu.co>
2021-11-25 11:32:40 -0500

    conectado a bd

--- src/connection/Connect.java
+++ src/connection/Connect.java
@@ -1,17 +1,31 @@
 
 package connection;
 
-import com.mysql.cj.protocol.Resultset;
-import java.sql.Statement;
 import java.sql.Connection;
 
 public class Connect {
     
-    String user = "root";
-    String pass = "1234";
-    String url = "jdbc:mysql://localhost:3306/node";
     Connection con;
-    Statement stmt;
-    Resultset rs;
+
+    public Connect() {
+        conectionBd();
+    }
+    
+    
+    public void conectionBd() {
+        String host = "localhost";
+        String user = "root";
+        String pass = "1234";
+        String bd = "hospital";
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String newConnectionURL = "jdbc:mysql://"+host+"/"+bd+"?"+"user="+user+"&password="+pass+"&useSSL=false";
+            con = DriverManager.getConnection( newConnectionURL );
+        } catch (Exception e) {
+            System.out.println("error al conectar"+ e);
+        }
+        
+    }
     
 }
